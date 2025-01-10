449e6457e20593f38f5a5b54dbca5caf9a7b99c0
Lakshitha Perera <eonzylakshitha@gmail.com>
2025-01-10 13:21:32 +0530

    Initial commit

---
+++ src/firebase-comment.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { collection, addDoc } from "@firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+};
+
+// Initialize with a unique name
+const app = initializeApp(firebaseConfig, 'comments-app');
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { db, storage, collection, addDoc };
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+import { collection, addDoc, getDocs } from "@firebase/firestore"; // Perbarui ini
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db, collection, addDoc };
